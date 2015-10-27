42e6ca52e1b1545dea407e9a52a24d9f48e2756a
Ignacio Ferrer <nachopad31@hotmail.com>
2015-10-27 20:30:08 +0100

    Actualizacionv2

--- www/js/index.js
+++ www/js/index.js
@@ -17,16 +17,14 @@
  * under the License.
  */
 var confBD = {
+  var existe_db="";
+  db="";
   initialize: function(){
-    var existe_db;
-    existe_db=window.localStorage.getItem("existe_db");
+    this.existe_db=window.localStorage.getItem("existe_db");
+    this.db=window.openDatabase("localDB","1.0","Base de datos",3*1024*1024);
     if(existe_db==null){
-      navigator.notification.confirm(
-        'La base de datos no existe', //Mensaje
-        this.onConfirm, //Confirm
-        'ERROR Base de datos', //Título
-        ['Crear','Salir']  //Array de botones
-      );
+      console.log("No existe la Base de datos");
+      this.createDB();
     }else{
       navigator.notification.alert(
           'Tienes una base de datos creada!',  // message
@@ -36,11 +34,36 @@ var confBD = {
       );
     }
   },
-  onConfirm:function(buttonIndex){
-    if(buttonIndex==1){
-      window.localStorage.setItem("existe_db",1);
-    }
-  }
+  createDB:function(){
+    console.log("Creamos la base de datos");
+    this.db.transaction(this.createLocalDB,this.createDBError,this.createDBSucc);
+  },
+  createLocalDB:function(tx){
+    var sql="CREATE TABLE IF NO EXIST localDB ("+
+            "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            "nombre VARCHAR(50),"+
+            "apellidos VARCHAR(256),"+
+            "cargo VARCHAR(128),"+
+            "email VARCHAR(64) )"
+        ;
+        tx.executeSql(sql);
+
+        sql="INSERT INTO localDB(nombre,apellidos,cargo,email)"+
+            "VALUES('Alex','de la Iglesia','Director','delaIglesia@gmail.com')";
+            tx.executeSql(sql);
+
+        sql="INSERT INTO localDB(nombre,apellidos,cargo,email)"+
+            "VALUES('Quentin','Tarantino','Director','quentin@gmail.com')";
+            tx.executeSql(sql);
+  },
+  createDBError:function(){
+
+  },
+  createLocalSucc:function(){
+    console.log("Se ha guardado la base de datos con éxito.");
+    window.localStorage.setItem("existe_db",1);
+  },
+
 };
 
 var app = {
